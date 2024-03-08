76e5cba9f01eb892492ed6c594a321608afe8632
stev3j <mygajeong@gmail.com>
2024-03-08 11:12:48 +0900

    design: component test

--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -1,21 +1,33 @@
-import React from "react"
-import { SafeAreaView } from "react-native"
+import React, { useState } from "react"
+import { View } from "react-native"
 import styled from "styled-components/native"
 import { LetsgoButton } from "../components/LetsgoButton"
+import { LetsgoTextInput } from "../components/LetsgoTextInput"
+import { colors } from "../styles/colors"
+import { PaddingView } from "../utils/PaddingView"
+import LetsgoTopBar from "../components/LetsgoTopBar"
 
 interface StartScreenProps {
     navigation : any
 }
 
 export const StartScreen : React.FC<StartScreenProps> = ({navigation}) => {
+    const [name, setName] = useState('')
 
     return (
-        <SafeAreaView>
-            <WelcomeText>This is StartScreen Screen</WelcomeText>
+        <Background>
+            <LetsgoTopBar title="로그인" onPress={() => {navigation.goBack()}}/>
             <LetsgoButton title="로그인" onPress={() => {navigation.navigate("Login")}}/>
-        </SafeAreaView>
-    )    
+            <View style={{height: 30}}/>
+            <LetsgoTextInput></LetsgoTextInput>
+        </Background>
+    );
 }
+
+const Background = styled.SafeAreaView`
+    flex: 1;
+    background-color: ${colors.white};
+`
 
 const WelcomeText = styled.Text`
     font-size: 10px;
