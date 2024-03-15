08a8218f8a60b0ce9deb0a92a053679662623bba
stev3j <mygajeong@gmail.com>
2024-03-15 11:26:51 +0900

    feat: add background, spacer util views

---
+++ src/utils/UtilViews.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+import { colors } from "../styles/colors";
+
+export const Background = styled.SafeAreaView`
+    flex: 1;
+    background-color: ${colors.white};
+`
+
+export const Spacer = styled.View`
+    flex: 1;
+`
