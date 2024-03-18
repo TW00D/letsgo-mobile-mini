a22e5802dfb8835b4d68bd28e30dd2946a48c6fe
조승완 <103572569+stev3j@users.noreply.github.com>
2024-03-18 11:37:12 +0900

    feat: using register function

--- src/services/AuthApi.ts
+++ src/services/AuthApi.ts
@@ -16,10 +16,11 @@ export function login(data: any, navigation: any) {
         });
 }
 
-export const register = (data: any) => {
+export const register = (data: any, navigation: any) => {
     axios.post(`${BASE_URL}/auth/register`, data)
         .then(response => {
             console.log(response.data);
+            navigation.navigate('Login')
         })
         .catch(error => {
             // if (axios.isAxiosError(error)) {
