42c9bd44391186f03f3950675c8dffff6021d9c1
조승완 <103572569+stev3j@users.noreply.github.com>
2024-03-18 12:14:41 +0900

    feat: add check point

--- src/screens/signup/SignupIdScreen.tsx
+++ src/screens/signup/SignupIdScreen.tsx
@@ -5,13 +5,15 @@ import { colors } from "../../styles/colors"
 import { LetsgoTextInput } from "../../components/LetsgoTextInput"
 import { LetsgoButton } from "../../components/LetsgoButton"
 import { Background, Spacer } from "../../utils/UtilViews"
+import CheckPoint from "../../components/CheckPoint"
+import { View } from "react-native"
 
 interface SignupIdScreenProps {
     navigation: any
 }
 
 const SignupIdScreen: React.FC<SignupIdScreenProps> = ({navigation}) => {
-    const [ isAbled, setAbled ] = useState(true) // 임시
+    const [ isOkay, setOkay ] = useState(false)
     const [ id, setId ] = useState('')
 
     // if (id.length > 0) {
@@ -23,9 +25,19 @@ const SignupIdScreen: React.FC<SignupIdScreenProps> = ({navigation}) => {
         <Background>
             <LetsgoTopBar title="" onPress={() => {navigation.goBack()}}/>
             <Title>희망하는 아이디를 입력해주세요</Title>
-            <LetsgoTextInput label="아이디" value={id} setValue={setId} isSecure={false}/>
+            <LetsgoTextInput 
+                label="아이디" 
+                value={id} 
+                setValue={setId} 
+                isSecure={false} 
+                onChange={() => {
+                    if (id.length >= 8 && id.length <= 20) setOkay(true)
+                    else setOkay(false)
+                }}/>
+            <View style={{height: 8}}/>
+            <CheckPoint title="8 ~ 20 자리" isOkay={isOkay}/>
             <Spacer/>
-            <LetsgoButton title="다음" isAbled={isAbled} onPress={() => {navigation.navigate('SignupPassword', {id: id})}}/>
+            <LetsgoButton title="다음" isAbled={isOkay} onPress={() => {navigation.navigate('SignupPassword', {id: id})}}/>
         </Background>
     );
 }
