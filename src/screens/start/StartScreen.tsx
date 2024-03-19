9ed3b6ea0b6bf213e57e4d473ba7d5aca4625b3b
조승완 <103572569+stev3j@users.noreply.github.com>
2024-03-19 18:17:06 +0900

    DEL: components in start screen
    안쓰는 것들 임포트에서 지움

--- src/screens/start/StartScreen.tsx
+++ src/screens/start/StartScreen.tsx
@@ -1,11 +1,8 @@
 import React from "react"
-import { Image, View } from "react-native"
+import { Image } from "react-native"
 import styled from "styled-components/native"
 import { LetsgoButton } from "../../components/button/LetsgoButton"
-import { LetsgoTextInput } from "../../components/LetsgoTextInput"
 import { colors } from "../../styles/colors"
-import { PaddingView } from "../../utils/PaddingView"
-import LetsgoTopBar from "../../components/topbar/LetsgoTopBar"
 
 interface StartScreenProps {
     navigation : any
