1a3089ca334e11e21d47fe5af7a960a318d27111
조승완 <103572569+stev3j@users.noreply.github.com>
2024-03-19 18:13:55 +0900

    FEAT: add components in PostModal
    모달 화면에 컴포넌트들 추가
    
    related: #14

--- src/screens/PostModal.tsx
+++ src/screens/PostModal.tsx
@@ -1,21 +1,40 @@
-import { View } from "react-native";
-import { Text } from "react-native-svg";
+import React, { useState } from "react";
 import styled from "styled-components/native";
 import ModalTopBar from "../components/topbar/ModalTopBar";
 import { colors } from "../styles/colors";
 import { useNavigation } from "@react-navigation/native";
 import PostButton from "../components/button/PostButton";
 import { Spacer } from "../utils/UtilViews";
+import TitleTextInput from "../components/textinput/TitleTextInput";
+import ContentTextInput from "../components/textinput/ContentTextInput";
+import PostingThemeList from "../components/PostingThemeList";
 
 const PostModal = () => {
+    const [ theme, setTheme ] = useState('패션')
+    const [ title, setTitle ] = useState('')
+    const [ content, setContent ] = useState('')
+
     return (
         <Background>
             <ModalTopBar/>
+            <PostingThemeList selected={theme} setSelect={setTheme}/>
+            <TitleTextInput 
+                value={title} 
+                setValue={setTitle} 
+                placeholder={`(${theme}) 제목을 입력하세요`}/>
+            <Line/>
+            <ContentTextInput 
+                value={content}
+                setValue={setContent} 
+                placeholder="욕설, 비방 등 상대방을 불쾌하게 하는 게시물은 게시하지 말아주세요. 신고를 당하면 커뮤니티 이용이 제한될 수 있어요."/>
+
             <Spacer/>
+            
             <ButtonFrame>
                 <Spacer/>
-                <PostButton isPostabled={false}/>
+                <PostButton isPostabled={(title.length > 0) && (content.length > 0)}/>
             </ButtonFrame>
+
             
         </Background>
     );
@@ -38,6 +57,14 @@ const ButtonFrame = styled.View`
     height: 72px;
     justify-content: center;
     margin-right: 16px;
+`
+
+const Line = styled.View`
+    height: 1px;
+    width: 100%;
+    background-color: ${colors.line_gray_50};
+    margin-top: 16px;
+    margin-left: 16px;
 `
 
 export default PostModal;
