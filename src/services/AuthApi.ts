9565650bd93b94c9d9410735f4fa27b6f833787d
조승완 <103572569+stev3j@users.noreply.github.com>
2024-03-19 09:56:29 +0900

    feat: login signup 에러링
    400에러 에러링
    
    related: #10

--- src/services/AuthApi.ts
+++ src/services/AuthApi.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import { BASE_URL } from './urls';
 import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 // 반복되는 코드가 많음?
 // - [ ] 사람들은 어떻게 axios를 사용하는지 확인하기
 
-export function login(data: any, navigation: any) {
+
+export const login = (data: any, navigation: any) => {
     axios.post(`${BASE_URL}/auth/login`, data)
         .then(response => {
             console.log(response.data);
@@ -13,6 +15,12 @@ export function login(data: any, navigation: any) {
         })
         .catch(error => {
             console.error("Error sending data: ", error);
+
+            if (error.response.status == 400) {
+                Alert.alert('아이디나 비밀번호를 다시 확인해주세요')
+            } else {
+                Alert.alert('서버 연결이 원활하지 않습니다.')
+            }
         });
 }
 
@@ -20,12 +28,15 @@ export const register = (data: any, navigation: any) => {
     axios.post(`${BASE_URL}/auth/register`, data)
         .then(response => {
             console.log(response.data);
-            navigation.navigate('Login')
+            navigation.navigate('Login' as never)
         })
         .catch(error => {
-            // if (axios.isAxiosError(error)) {
-            //     console.log(error.response?.data)
-            // }
             console.error("Error sending data: ", error);
+
+            if (error.response.status == 400) {
+                Alert.alert('아이디가 중복되었는지 확인해주세요')
+            } else {
+                Alert.alert('서버 연결이 원활하지 않습니다.')
+            }
         });
 }
