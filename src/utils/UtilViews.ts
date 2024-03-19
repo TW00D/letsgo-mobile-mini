3685b15234ca6fa3d4ea985a4f9d6eec5a233b68
조승완 <103572569+stev3j@users.noreply.github.com>
2024-03-19 16:04:29 +0900

    refactor: row & columns
    상현이가 추가시켜줌!!

--- src/utils/UtilViews.ts
+++ src/utils/UtilViews.ts
@@ -9,3 +9,45 @@ export const Background = styled.SafeAreaView`
 export const Spacer = styled.View`
     flex: 1;
 `
+
+export type Align = "center" | "flex-start" | "flex-end";
+
+export type FlexProps = {
+  gap?: number;
+  columnGap?: number;
+  rowGap?: number;
+  justifyContent?: Align | "space-between";
+  fill?: string;
+  alignItems?: Align;
+  padding?: [number] | [number, number];
+  wrap?: boolean;
+  backgroundColor?: string;
+};
+
+export const BaseFlex = styled.View<FlexProps>`
+  display: flex;
+
+  gap: ${({ gap }) => gap || 0}px;
+  row-gap: ${({ rowGap }) => rowGap || 0}px;
+  column-gap: ${({ columnGap }) => columnGap || 0}px;
+
+  justify-content: ${({ justifyContent }) => justifyContent || "flex-start"};
+  align-items: ${({ alignItems }) => alignItems || "flex-start"};
+  flex-wrap: ${({ wrap }) => (wrap ? "wrap" : "nowrap")};
+
+  width: ${({ fill }) => fill || "auto"};
+  background-color: ${({ backgroundColor }) =>
+    backgroundColor || "transparent"};
+  padding: ${({padding}) =>
+    `${padding ? padding[0] : 0}px ${
+      padding ? padding[1] ?? padding[0] : 0
+    }px`};
+`;
+
+export const Column = styled(BaseFlex)`
+  flex-direction: column;
+`;
+
+export const Row = styled(BaseFlex)`
+  flex-direction: row;
+`;
