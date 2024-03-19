8ea07d7ca1c76d3f6a29a8145d037478e853fec5
조승완 <103572569+stev3j@users.noreply.github.com>
2024-03-19 13:59:18 +0900

    FEAT: add Post Modal
    모달 화면 개발
    
    related: #14

---
+++ src/screens/PostModal.tsx
@@ -0,0 +1,28 @@
+import { View } from "react-native";
+import { Text } from "react-native-svg";
+import styled from "styled-components/native";
+import ModalTopBar from "../components/ModalTopBar";
+import { colors } from "../styles/colors";
+import { useNavigation } from "@react-navigation/native";
+
+const PostModal = () => {
+    return (
+        <Background>
+            <ModalTopBar/>
+        </Background>
+    );
+}
+
+const Background = styled.View`
+    flex: 1;
+    background-color: ${colors.white};
+`
+
+const Title = styled.Text`
+    font-size: 18px;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`
+
+export default PostModal;
