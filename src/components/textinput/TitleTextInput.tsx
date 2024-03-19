987b0179232de6cdce85bf23b74c2467dc5f61c7
조승완 <103572569+stev3j@users.noreply.github.com>
2024-03-19 16:05:24 +0900

    FEAT: add TitleTextInput component
    컴포넌트 추가
    
    related: #14

---
+++ src/components/textinput/TitleTextInput.tsx
@@ -0,0 +1,39 @@
+import { Dispatch, SetStateAction } from "react";
+import styled from "styled-components/native";
+import { colors } from "../../styles/colors";
+import { View } from "react-native";
+
+type TitleTextInputType = {
+    value: string,
+    setValue: Dispatch<SetStateAction<string>>,
+    placeholder: string
+}
+
+const TitleTextInput = ({value, setValue, placeholder}: TitleTextInputType) => {
+
+    const onChangeText = (text: string) => {
+        setValue(text)
+    }
+
+    return (
+        <View>
+            <TextInput
+                value={value}
+                onChangeText={onChangeText}
+                placeholder={placeholder}/>
+            
+        </View>
+        
+        
+    );
+}
+
+const TextInput = styled.TextInput`
+    color: ${colors.text_gray_900};
+    font-size: 20px;
+    margin-left: 16px;
+    margin-top: 24px;
+    margin-right: 16px;
+`
+
+export default TitleTextInput;
