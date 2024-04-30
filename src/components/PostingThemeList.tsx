968a7de4a69b571d99134fb09c17d0a607decd9e
조승완 <103572569+stev3j@users.noreply.github.com>
2024-05-01 08:40:29 +0900

    fix: category change

--- src/components/PostingThemeList.tsx
+++ src/components/PostingThemeList.tsx
@@ -9,30 +9,18 @@ type PostingThemeListType = {
 }
 
 const PostingThemeList = ({selected, setSelect}: PostingThemeListType) => {
-    const fashionIcon = selected == '패션' ? require('../assets/images/posting-buttons/img_fashion_clicked.png') : require('../assets/images/posting-buttons/img_fashion.png')
-    const studyIcon = selected == '공부' ? require('../assets/images/posting-buttons/img_study_clicked.png') : require('../assets/images/posting-buttons/img_study.png')
-    const musicIcon = selected == '덕질' ? require('../assets/images/posting-buttons/img_music_clicked.png') : require('../assets/images/posting-buttons/img_music.png')
     const animeIcon = selected == '애니' ? require('../assets/images/posting-buttons/img_anime_clicked.png') : require('../assets/images/posting-buttons/img_anime.png')
-    const gameIcon = selected == '게임' ? require('../assets/images/posting-buttons/img_game_clicked.png') : require('../assets/images/posting-buttons/img_game.png')
     const loveIcon = selected == '연애' ? require('../assets/images/posting-buttons/img_love_clicked.png') : require('../assets/images/posting-buttons/img_love.png')
     const strengthIcon = selected == '운동' ? require('../assets/images/posting-buttons/img_strength_clicked.png') : require('../assets/images/posting-buttons/img_strength.png')
+    const fashionIcon = selected == '패션' ? require('../assets/images/posting-buttons/img_fashion_clicked.png') : require('../assets/images/posting-buttons/img_fashion.png')
+    const gameIcon = selected == '게임' ? require('../assets/images/posting-buttons/img_game_clicked.png') : require('../assets/images/posting-buttons/img_game.png')
+    const studyIcon = selected == '공부' ? require('../assets/images/posting-buttons/img_study_clicked.png') : require('../assets/images/posting-buttons/img_study.png')
+    const musicIcon = selected == '덕질' ? require('../assets/images/posting-buttons/img_music_clicked.png') : require('../assets/images/posting-buttons/img_music.png')
 
     return (
         <Row style={{marginTop: 24, marginLeft: 16}}>
-            <TouchableOpacity onPress={() => { setSelect('패션') }}>
-                <ThemeIcon source={fashionIcon}/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => { setSelect('공부') }}>
-                <ThemeIcon source={studyIcon}/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => { setSelect('덕질') }}>
-                <ThemeIcon source={musicIcon}/>
-            </TouchableOpacity>
             <TouchableOpacity onPress={() => { setSelect('애니') }}>
                 <ThemeIcon source={animeIcon}/>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => { setSelect('게임') }}>
-                <ThemeIcon source={gameIcon}/>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => { setSelect('연애') }}>
                 <ThemeIcon source={loveIcon}/>
@@ -40,6 +28,23 @@ const PostingThemeList = ({selected, setSelect}: PostingThemeListType) => {
             <TouchableOpacity onPress={() => { setSelect('운동') }}>
                 <ThemeIcon source={strengthIcon}/>
             </TouchableOpacity>
+            <TouchableOpacity onPress={() => { setSelect('패션') }}>
+                <ThemeIcon source={fashionIcon}/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => { setSelect('게임') }}>
+                <ThemeIcon source={gameIcon}/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => { setSelect('공부') }}>
+                <ThemeIcon source={studyIcon}/>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => { setSelect('덕질') }}>
+                <ThemeIcon source={musicIcon}/>
+            </TouchableOpacity>
+            
+            
+            
+            
+            
         </Row>
     );
 }
